50decc6bc2e665c65c9bff94b192a5d549130491
Shivraj Kumar <shivrajkumar11math@gmail.com>
2024-01-30 00:41:24 +0530

    Hero Section

---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HeroImage from "../../assets/hero-img.png";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={HeroImage} alt="headphones" width={212} />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
