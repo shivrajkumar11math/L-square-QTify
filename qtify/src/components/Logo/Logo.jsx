a682136845a0c7d5fca12f7a21c83f000e0364a7
SHIVRKUM <SHIVRKUM@SHIVRKUM-T490.us.oracle.com>
2024-01-29 17:10:50 +0530

    changing the logo margin

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LogoImage from "../../assets/logoImage.png";
+import styles from "./Logo.module.css";
 
 const Logo = () => {
   return (
     <img
+      className={styles.logo}
       src={LogoImage}
       alt="logo"
       width={67}
