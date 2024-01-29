f0d81ee693dc25452334301cc92f31feb8823c80
SHIVRKUM <SHIVRKUM@SHIVRKUM-T490.us.oracle.com>
2024-01-29 16:51:13 +0530

    Fixed the button font family and navbar logo height

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import LogoImage from "../../assets/logoImage.png";
 
 const Logo = () => {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return (
+    <img
+      src={LogoImage}
+      alt="logo"
+      width={67}
+      height={34}
+    />
+  );
 };
 
 export default Logo;
